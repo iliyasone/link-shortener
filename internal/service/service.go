87d76bcaed4c3d8458d58ee107dea4ada48d7fab
iliyasone <iliyas.dzabbarov@gmail.com>
2025-02-10 15:00:22 +0300

    docs: add comment

---
+++ internal/service/service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"errors"
+	"link-shortener/internal/storage"
+	"link-shortener/pkg/generator"
+)
+
+func SaveURL(store storage.Storage, originalURL string) (string, error) {
+    if existing, err := store.FindByOriginal(originalURL); err == nil {
+		return existing, nil
+	}
+
+	const maxRetries = 1000
+	defaultGenerator := generator.NewGenerator()
+	for i := 0; i < maxRetries; i++ {
+		candidate, err := defaultGenerator.Generate()
+		if err != nil {
+			return "", err
+		}
+		err = store.Save(originalURL, candidate)
+		if err == nil {
+			return candidate, nil
+		}
+		if errors.Is(err, storage.ErrShortURLExists) {
+			continue
+		}
+        // another error, return
+		return "", err
+	}
+
+	return "", errors.New("failed to generate a unique short URL after many attempts")
+}

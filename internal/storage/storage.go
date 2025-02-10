8865fb65ef2d9b03b62e765b666fc909c191fca6
iliyasone <iliyas.dzabbarov@gmail.com>
2025-02-10 15:00:49 +0300

    docs: add comment

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,12 @@
 package storage
 
 type Storage interface {
+    // Unsafe save a new short URL in the storage
+    //
+    // Note: does not check for uniqueness in either direction
     Save(originalURL, shortURL string) error
+    // Get original URL by short URL
     Get(shortURL string) (string, error)
+    // Checks if the URL already saved in storage
     FindByOriginal(originalURL string) (string, error)
 }

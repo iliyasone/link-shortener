0b3b7e973bbc8a93fd124a758c3320771833ff13
iliyasone <iliyas.dzabbarov@gmail.com>
2025-02-10 14:56:23 +0300

    core: storage abstraction now has a FindByOriginal

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,4 +3,5 @@ package storage
 type Storage interface {
     Save(originalURL, shortURL string) error
     Get(shortURL string) (string, error)
+    FindByOriginal(originalURL string) (string, error)
 }
